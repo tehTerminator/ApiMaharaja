a6016d7c4dfe3b2ba1a7067554c27f821a7b5ed3
Prateek Kher <prateek.kher@gmail.com>
2022-10-26 21:41:16 +0530

    Created UserController.php [Unfinished]

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    function authenticate(string $username, string $password) {
+
+    }
+}
