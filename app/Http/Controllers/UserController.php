481742060cb4381e5ddccde28e44622f86a33268
Prateek Kher <prateek.kher@gmail.com>
2022-10-28 20:57:26 +0530

    added authenticate() method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\UserService;
+use App\Services\ValidationService;
+use Illuminate\Http\Request;
+
 class AuthController extends Controller
 {
     /**
@@ -14,7 +18,23 @@ class AuthController extends Controller
         //
     }
 
-    function authenticate(string $username, string $password) {
+    public function authenticate(Request $request)
+    {
+        $rules = ValidationService::getValidationRules(
+            'users',
+            ['email', 'password'],
+            false
+        );
+        $this->validate($request, $rules);
+        $user = UserService::authenticate(
+            $request->email,
+            $request->password
+        );
+        return response()->json($user);
+    }
 
+    public function store(Request $request)
+    {
+        
     }
 }
