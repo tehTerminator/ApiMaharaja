9375ddcbd0bc176c0adcd8bed4efad28d4b74c0b
Prateek Kher <prateek.kher@gmail.com>
2022-11-01 17:01:17 +0530

    Added scopes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model {
@@ -12,6 +13,34 @@ class Task extends Model {
     ];
 
     public function customer() {
-        $this->belongsTo(Customer::class);
+        return $this->belongsTo(Customer::class, 'customer_id', 'id');
+    }
+
+    public function createdBy() {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
+
+    public function acceptedBy() {
+        return $this->belongsTo(User::class, 'accepted_by', 'id');
+    }
+
+    public function scopePending($query) {
+        return $query->where('state', 'PENDING');
+    }
+
+    public function scopeRejected($query) {
+        return $query->where('state', 'REJECTED');
+    }
+
+    public function scopeAccepted($query) {
+        return $query->where('state', 'ACCEPTED');
+    }
+
+    public function scopeCompleted($query) {
+        return $query->where('state', 'COMPLETED');
+    }
+
+    public function scopeToday($query) {
+        return $query->whereDate('created_at', Carbon::today());
     }
 }
